8b7634a7256da38d920bac6c62422af35caaf77b
lupenq <skinilya@gmail.com>
2020-04-14 08:30:06 +0300

    Edit githubsearch/

---
+++ githubsearch/precache-manifest.62c0dff6ce819c84b28fe07043f5614f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7085b3f4977ab8d1b990c60667f89016",
+    "url": "/githubsearch/index.html"
+  },
+  {
+    "revision": "c727a0f83fcdc7cf89e5",
+    "url": "/githubsearch/static/css/main.ff0d9e58.chunk.css"
+  },
+  {
+    "revision": "6411f03004d14e7aa9bd",
+    "url": "/githubsearch/static/js/2.9aa367eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/githubsearch/static/js/2.9aa367eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c727a0f83fcdc7cf89e5",
+    "url": "/githubsearch/static/js/main.9f81cea2.chunk.js"
+  },
+  {
+    "revision": "0ac35ef4dc1998d72d18",
+    "url": "/githubsearch/static/js/runtime-main.4ae485c9.js"
+  }
+]);
